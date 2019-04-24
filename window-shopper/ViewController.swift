ca7342a86f4e4b9b72490e5e68a9b41031c7c781
akmal5055 <akmal_5055@icloud.com>
2019-04-24 01:09:39 -0400

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Akmal Sanginov on 4/24/19.
+//  Copyright © 2019 Sanginov5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
